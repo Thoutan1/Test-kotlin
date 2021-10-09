a1222a02e9f658f4674c2e105abba762db0b07dc
Tudo12 <gfirman923@gmail.com>
2021-10-09 23:50:19 +0700

    Initial Commit

---
+++ src/main.kt
@@ -0,0 +1,6 @@
+import module.main.hello
+
+fun main(){
+    hello()
+    print("OK")
+}
---
+++ src/module.kt
@@ -0,0 +1,9 @@
+package module.main
+import java.util.Scanner
+
+fun hello(){
+    println("Before print Hello please enter your name: ")
+    val input = Scanner(System.`in`)
+    val name = input.next()
+    println("Hello $name")
+}
